5b82c4713477816d49dd9bb70d99a9e6258e17bc
Alejandro M <diegomartinez1101@gmail.com>
2023-06-08 16:22:12 -0500

    Coreccion para entrar a el producto

--- index.py
+++ index.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
