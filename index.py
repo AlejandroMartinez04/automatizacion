35e70a761b4cb8bdb331c17b78dd97693a52654e
Alejandro M <diegomartinez1101@gmail.com>
2023-06-08 16:01:41 -0500

    Coreccion para entrar a el producto

--- index.py
+++ index.py
@@ -95,10 +95,16 @@ print(texto7, texto8, texto9)
 print(texto10, texto11, texto12)
 time.sleep(5)
 
+# ingresa a uno de los productos buscados
+apuntador = driver.find_element(By.XPATH,"//*[@id='gallery-layout-container']/div[1]")
+apuntador.click()
+time.sleep(3)
+
 # Se realiza el scroll hacia abajo y se a click en el icono youtube
+time.sleep(5)
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 time.sleep(3)
-apuntador = driver.find_element(By.XPATH,"/html/body/div[2]/div/div[1]/div/div[6]/div/div/div[2]/section/div/div[3]/div/div[2]/div/div[1]/div[2]/a[4]")
+apuntador = driver.find_element(By.XPATH,"/html/body/div[2]/div/div[1]/div/div/div/div[6]/div/div/div[2]/section/div/div[3]/div/div[2]/div/div[1]/div[2]/a[4]")
 apuntador.click()
 time.sleep(8)
 
